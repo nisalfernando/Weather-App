161957773b08a405ac14ad410f6ffd4e95a8f7cb
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-09-04 15:55:08 +0200

    Selected p tag with location class

--- app.js
+++ app.js
@@ -2,3 +2,4 @@
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
