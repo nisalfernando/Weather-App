e672c36dc568435dea7c7891e2c949430d6ec0e8
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-09-05 18:00:28 +0200

    Selecting html elements

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
