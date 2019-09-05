ca9ead8832e06440e6286f27570f16811627b26c
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-09-05 18:04:10 +0200

    Created an empty object

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 const notificationElement = document.querySelector(".notification");
+
+// App data
+const weather = {};
