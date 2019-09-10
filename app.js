c82775d53b44457fe761dc0fd341339eb7bdf126
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-09-10 19:25:00 +0200

    Finished the IF statement

--- app.js
+++ app.js
@@ -21,4 +21,8 @@ const key = "82005d27a116c2880c8f0fcb866998a0";
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
 if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML =
+        "<p>Browser doesn't Support Geolocation</p>";
 }
